7cca61d0559b825956f4e5cb1b7a01b6bdd53ade
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-02-20 04:37:52 +0000

    finish practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,39 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int lettersToFive(String s, char c) {
+		int iterator = 0, count = 0;
+		while(iterator < s.length()) {
+			
+			if(s.charAt(iterator) == c)
+				count++;
+			iterator++;
+			
+			if(count == 5)
+				return iterator;
+		}
+		return -1;
+	}
+	public static int countLetter(String s, char c) {
+		int count = 0;
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) ==  c)
+				count++;
+		}
+		return count;
+	}
+	public static int oddSum(int num1, int num2) {
+		int total = 0;
+
+		int startNum = Math.min(num1, num2);
+		int finishNum = Math.max(num1, num2);
+
+		for(int i = startNum; i <= finishNum; i++) {
+			if(i % 2 != 0)
+				total+=i;
+		}
+		return total;
+	}
+
 
 }
